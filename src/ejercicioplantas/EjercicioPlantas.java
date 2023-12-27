d2b209b7447734212086394d67be7ae92a1238fe
terrahsa@gmail.com <118025389+AdrianB378@users.noreply.github.com>
2023-12-27 20:37:31 -0300

    Se agrega el proyecto

---
+++ src/ejercicioplantas/EjercicioPlantas.java
@@ -0,0 +1,33 @@
+
+package ejercicioplantas;
+
+import logica.Arbol;
+import logica.Arbusto;
+import logica.Flor;
+
+
+
+
+
+
+public class EjercicioPlantas {
+
+    public static void main(String[] args) {
+        
+        
+        Arbol arbol = new Arbol();
+        Flor flor = new Flor();
+        Arbusto arbus = new Arbusto();
+        
+        arbol.decirLoQueSoy();
+        flor.decirLoQueSoy();
+        arbus.decirLoQueSoy();
+        
+        
+        
+        
+       
+        
+    }
+    
+}
---
+++ src/logica/Flor.java
@@ -0,0 +1,79 @@
+
+package logica;
+
+public class Flor extends Planta{
+    
+    private String colorPetalos;
+    private int cantPetalos; // promedio cantidad de petalos
+    private String colorPistilo;
+    private String variedadFlor;
+    private String estacionFlorece;
+
+    public Flor() {
+    }
+
+    public Flor(String colorPetalos, int cantPetalos, String colorPistilo, String variedadFlor, String estacionFlorece, String nombre, double altoTallo, boolean tieneHojas, String climaIdeal) {
+        super(nombre, altoTallo, tieneHojas, climaIdeal);
+        this.colorPetalos = colorPetalos;
+        this.cantPetalos = cantPetalos;
+        this.colorPistilo = colorPistilo;
+        this.variedadFlor = variedadFlor;
+        this.estacionFlorece = estacionFlorece;
+    }
+
+    public String getColorPetalos() {
+        return colorPetalos;
+    }
+
+    public void setColorPetalos(String colorPetalos) {
+        this.colorPetalos = colorPetalos;
+    }
+
+    public int getCantPetalos() {
+        return cantPetalos;
+    }
+
+    public void setCantPetalos(int cantPetalos) {
+        this.cantPetalos = cantPetalos;
+    }
+
+    public String getColorPistilo() {
+        return colorPistilo;
+    }
+
+    public void setColorPistilo(String colorPistilo) {
+        this.colorPistilo = colorPistilo;
+    }
+
+    public String getVariedadFlor() {
+        return variedadFlor;
+    }
+
+    public void setVariedadFlor(String variedadFlor) {
+        this.variedadFlor = variedadFlor;
+    }
+
+    public String getEstacionFlorece() {
+        return estacionFlorece;
+    }
+
+    public void setEstacionFlorece(String estacionFlorece) {
+        this.estacionFlorece = estacionFlorece;
+    }
+
+    
+    
+    
+    
+
+    @Override
+    public void decirLoQueSoy() {
+        System.out.println("Hola soy una Flor");    
+    }
+
+    
+    
+    
+    
+    
+}
